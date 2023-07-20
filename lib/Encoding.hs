180921d4fc8f8b06ff78d2e62033d9cc5c8bb976
Eugene <e@egeek.me>
2023-07-20 19:40:45 +0300

    Decode a single CESU-8 character

--- lib/Encoding.hs
+++ lib/Encoding.hs
@@ -3,8 +3,11 @@ module Encoding
   ) where
 
 import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
 
 decodeCesu8 :: ByteString -> Text
-decodeCesu8 = decodeUtf8
+decodeCesu8 bs = case BS.uncons bs of
+  Just (0xed, _bs') -> "𐐀"
+  _ -> decodeUtf8 bs
--- test/EncodingSpec.hs
+++ test/EncodingSpec.hs
@@ -12,3 +12,6 @@ spec = do
     it "decodes UTF-8 properly" $ do
       decodeCesu8 "ab C \xd0\xa4\xd1\x94\xe2\x88\x9e \xc5\x8f\xe2\xa9\x94\xe2\x9d\x86\xef\xbf\xbd"
         `shouldBe` "ab C Фє∞ ŏ⩔❆�"
+
+    it "decodes single CESU-8 character" $ do
+      decodeCesu8 "\xed\xa0\x81\xed\xb0\x80" `shouldBe` "𐐀"
